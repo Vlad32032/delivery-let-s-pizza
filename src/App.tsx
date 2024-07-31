0efcb27ee3b689701a88abec628091f3a9bdf3db
Vlad32032 <vlad32032@mail.ru>
2024-08-01 02:02:49 +0300

    remove lazy load

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 
-import React, { Suspense } from "react";
+import React from "react";
 
 import { Route, Routes } from "react-router-dom";
 
 import MainLayouts from "./layouts/MainLayouts";
 
 import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import FullPizza from "./pages/FullPizza";
+import NotFound from "./pages/NotFound";
 
 import "./scss/app.scss";
 
-const Cart = React.lazy(() => import(/* webpackChunkName: "Cart" */ './pages/Cart'));
-const FullPizza = React.lazy(() => import(/* webpackChunkName: "FullPizza" */ './pages/FullPizza'));
-const NotFound = React.lazy(() => import(/* webpackChunkName: "NotFound" */ './pages/NotFound'));
+// const Cart = React.lazy(() => import(/* webpackChunkName: "Cart" */ './pages/Cart'));
+// const FullPizza = React.lazy(() => import(/* webpackChunkName: "FullPizza" */ './pages/FullPizza'));
+// const NotFound = React.lazy(() => import(/* webpackChunkName: "NotFound" */ './pages/NotFound'));
 
 const App: React.FC = () => {
 	return (
 		<div className="App">
 			<Routes>
-				<Route path="/diliveri-let-s-pizza/" element={<MainLayouts />}>
+				{/* <Route path="/diliveri-let-s-pizza/" element={<MainLayouts />}>
 					<Route path="" element={<Home />} />
 					<Route path="cart" element={
 						<Suspense fallback={<div>Идет загрузка...</div>}>
@@ -39,7 +42,15 @@ const App: React.FC = () => {
 						<Suspense fallback={<div>Идет загрузка...</div>}>
 							<NotFound />
 						</Suspense>
-					} />
+					} /> */}
+
+				<Route path="/diliveri-let-s-pizza/" element={<MainLayouts />}>
+					<Route path="" element={<Home />} />
+					<Route path="cart" element={<Cart />} />
+					<Route path="pizza/:id" element={<FullPizza />} />
+					<Route path="*" element={<NotFound />} />
+				</Route>
+				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</div>
 	);
